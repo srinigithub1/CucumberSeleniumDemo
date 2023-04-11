54a05b71f4deb4e4356496f00fed450d6c23e67c
SriniGitHub <40481341+srinigithub1@users.noreply.github.com>
2023-04-11 21:14:05 +0530

    this is commit on 11 Apr 23 at 21.14 PM IST

--- src/test/java/selneiumTests/CucumberRunnerTests.java
+++ src/test/java/selneiumTests/CucumberRunnerTests.java
@@ -2,7 +2,7 @@ package selneiumTests;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-    
+    // this is comment to trigger build in jenkins
 @CucumberOptions(tags = "", features="Features",glue={"StepDefinition"},
                  plugin = {},	
                  publish=true)
